e90b5633f28ddd55a226f0e4f512dfafac70dcde
FernandoHenriqueSouza <fernando_sousa1992@hotmail.com>
2023-08-02 23:53:41 -0300

    correcao 3

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_object" "codigo_spark" {
   bucket = aws_s3_bucket.datalake.id
-  key    = "emr-code/pyspark/job_spark_from_tf.py"
+  key    = "emr-code-dois/pyspark/job_spark_from_tf.py"
   acl    = "private"
   source = "../job_spark.py"
   etag   = filemd5("../job_spark.py")
